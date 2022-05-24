59bc82417e4dc6e332a9ad5cc84650ed016900cd
Wafa Bergaoui <wafa.bergaoui@sastec-group.com>
2022-05-24 15:27:21 +0100

    change videos visibility

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import { PlayerView } from "../components/views";
 
-export default function Home() {
-  //console.log(data);
+export default function Home({data}) {
+  console.log(data);
   return (
     <div>
       <PlayerView />
@@ -9,11 +9,11 @@ export default function Home() {
   );
 }
 
-// export async function getServerSideProps() {
-//   const res = await fetch(`https://cloudflare-test-ajl.pages.dev/api/videos/`, {
-//     headers: { accept: "application/json" },
-//   });
-//   const data = await res.json();
+export async function getServerSideProps() {
+  const res = await fetch(`https://cloudflare-test-ajl.pages.dev/api/videos/`, {
+    headers: { accept: "application/json" },
+  });
+  const data = await res.json();
 
-//   return { props: { data } };
-// }
+  return { props: { data } };
+}
