6a65adcd2f075d86a63999badcd7a3aa901797fb
Wafa Bergaoui <wafa.bergaoui@sastec-group.com>
2022-05-24 14:58:13 +0100

    add client side

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,20 @@
 import { PlayerView } from "../components/views";
+import { useEffect } from "react";
 
-export default function Home() {
+export default function Home({ data }) {
+  useEffect = () => console.log(data);
   return (
     <div>
       <PlayerView />
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const res = await fetch("/api/videos/", {
+    headers: { accept: "application/json" },
+  });
+  const data = await res.json();
+
+  return { props: { data } };
 }
