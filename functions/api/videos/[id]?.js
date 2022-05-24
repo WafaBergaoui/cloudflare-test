3616abdfd187048ecd8614602448e632d4588273
Wafa Bergaoui <wafa.bergaoui@sastec-group.com>
2022-05-24 08:53:42 +0100

    fix errors

--- functions/api/videos/[id]?.js
+++ functions/api/videos/[id]?.js
@@ -34,45 +34,46 @@ export async function onRequestGet(context) {
         },
       }
     );
-  } else {
-    const url = new URL(request.url);
-    const res = await (
-      await fetch(
-        `https://api.cloudflare.com/client/v4/accounts/${
-          env.CF_ACCOUNT_ID
-        }/stream?search=${url.searchParams.get("search") || ""}`,
-        {
-          headers: {
-            Authorization: `Bearer ${env.CF_API_TOKEN_STREAM}`,
-          },
-        }
-      )
-    ).json();
+  } 
+  // else {
+  //   const url = new URL(request.url);
+  //   const res = await (
+  //     await fetch(
+  //       `https://api.cloudflare.com/client/v4/accounts/${
+  //         env.CF_ACCOUNT_ID
+  //       }/stream?search=${url.searchParams.get("search") || ""}`,
+  //       {
+  //         headers: {
+  //           Authorization: `Bearer ${env.CF_API_TOKEN_STREAM}`,
+  //         },
+  //       }
+  //     )
+  //   ).json();
 
-    const filteredVideos = res.result.filter(
-      (x) => x.meta.visibility === "public"
-    );
-    const videos = await Promise.all(
-      filteredVideos.map(async (x) => {
-        const signedId = await getSignedStreamId(
-          x.uid,
-          env.CF_STREAM_SIGNING_KEY
-        );
-        return {
-          uid: x.uid,
-          status: x.status,
-          thumbnail: `https://videodelivery.net/${signedId}/thumbnails/thumbnail.jpg`,
-          meta: {
-            name: x.meta.name,
-          },
-          created: x.created,
-          modified: x.modified,
-          duration: x.duration,
-        };
-      })
-    );
-    return new Response(JSON.stringify(videos), {
-      headers: { "content-type": "application/json" },
-    });
-  }
+  //   const filteredVideos = res.result.filter(
+  //     (x) => x.meta.visibility === "public"
+  //   );
+  //   const videos = await Promise.all(
+  //     filteredVideos.map(async (x) => {
+  //       const signedId = await getSignedStreamId(
+  //         x.uid,
+  //         env.CF_STREAM_SIGNING_KEY
+  //       );
+  //       return {
+  //         uid: x.uid,
+  //         status: x.status,
+  //         thumbnail: `https://videodelivery.net/${signedId}/thumbnails/thumbnail.jpg`,
+  //         meta: {
+  //           name: x.meta.name,
+  //         },
+  //         created: x.created,
+  //         modified: x.modified,
+  //         duration: x.duration,
+  //       };
+  //     })
+  //   );
+  //   return new Response(JSON.stringify(videos), {
+  //     headers: { "content-type": "application/json" },
+  //   });
+  // }
 }
