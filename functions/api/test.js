0ef3227587bebecbedc9aab82dafa74f7f843ac3
Wafa Bergaoui <wafa.bergaoui@sastec-group.com>
2022-05-24 09:57:03 +0100

    test api

--- functions/api/test.js
+++ functions/api/test.js
@@ -1,10 +1,17 @@
 // GET requests to /filename would return "Hello, world!"
-export const onRequestGet = () => {
-    return new Response("This is our API TEST")
-  }
-  
-  // POST requests to /filename with a JSON-encoded body would return "Hello, <name>!"
-  export const onRequestPost = async ({ request }) => {
-    const { name } = await request.json()
-    return new Response(`Hello, ${name}!`)
-  }
+export const onRequestGet = async () => {
+  let data = [];
+  await fetch("https://jsonplaceholder.typicode.com/todos/1")
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json);
+      data = json;
+    });
+  return new Response(data);
+};
+
+// POST requests to /filename with a JSON-encoded body would return "Hello, <name>!"
+export const onRequestPost = async ({ request }) => {
+  const { name } = await request.json();
+  return new Response(`Hello, ${name}!`);
+};
