f6475976f86eb6f84ff32d7e4657b4e7f0197f53
Wafa Bergaoui <wafa.bergaoui@sastec-group.com>
2022-05-25 09:45:34 +0100

    change cfStream method

--- utils/cfStream.js
+++ utils/cfStream.js
@@ -3,36 +3,46 @@ const expiresTimeInS = 3600;
 
 export const getSignedStreamId = async (id, jwkKey) => {
   const encoder = new TextEncoder();
-  const expiresIn = Math.floor(Date.now() / 1000) + expiresTimeInS;
+  const expiresIn = Math.floor(Date.now() / 1000) + expiresTimeInS
   const jwk = JSON.parse(window.atob(jwkKey));
 
   const headers = {
     alg: "RS256",
-    kid: jwk.kid,
+    kid: "3fd02889f63237bb736e21e33eb07a7a",
   };
 
   const data = {
-    sub: id,
-    kid: jwk.kid,
-    exp: expiresIn,
+    "sub": id,
+    "kid": jwk.kid,
+    "exp": expiresIn,
+    "accessRules": [
+      {
+        "type": "ip.geoip.country",
+        "action": "allow",
+        "country": [
+          "GB"
+        ]
+      },
+      {
+        "type": "any",
+        "action": "block"
+      }
+    ]
   };
 
   const token = `${objectToBase64url(headers)}.${objectToBase64url(data)}`;
 
   const key = await crypto.subtle.importKey(
-    "jwk",
-    jwk,
+    "jwk", jwk,
     {
       name: "RSASSA-PKCS1-v1_5",
       hash: "SHA-256",
     },
-    false,
-    ["sign"]
+    false, ["sign"]
   );
 
   const signature = await crypto.subtle.sign(
-    { name: "RSASSA-PKCS1-v1_5" },
-    key,
+    { name: "RSASSA-PKCS1-v1_5" }, key,
     encoder.encode(token)
   );
 
