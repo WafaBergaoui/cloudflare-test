a93450bac617147ba18817fa65bf8865e1b91eff
Wafa Bergaoui <wafa.bergaoui@sastec-group.com>
2022-05-25 11:22:11 +0100

    change cfStream method

--- components/views/PlayerView/template.jsx
+++ components/views/PlayerView/template.jsx
@@ -2,20 +2,20 @@ import React from 'react'
 import { Stream } from "@cloudflare/stream-react";
 import S from './style.module.scss';
 
-const PlayerView = ({videos}) => {
+const PlayerView = () => {
 
   //const videoID = videoId
   const videoID = '548a1762f17f8ea1cda8344e0e48de7a';
 
-  const getVideosId = () => {
-    videos.map((video) => {return video.uid});
-  }
+  // const getVideosId = () => {
+  //   videos.map((video) => {return video.uid});
+  // }
 
   videos.map((video) => {
     console.log(video.uid);
     return (
       <div className={S.Player}>
-        <Stream controls src={video.uid}/>
+        <Stream controls src={videoID}/>
       </div>
     )
   })
