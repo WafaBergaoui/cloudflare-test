83d1473b853ed14a4f1e6d7032ebc084d5a616a0
Wafa Bergaoui <wafa.bergaoui@sastec-group.com>
2022-05-25 08:41:24 +0100

    add admin api

--- functions/api/videos/[id]?.js
+++ functions/api/videos/[id]?.js
@@ -21,11 +21,12 @@ export async function onRequestGet(context) {
     //   return new Response(null, { status: 401 });
     //}
 
-    const signedId = await getSignedStreamId(id, env.CF_STREAM_SIGNING_KEY);
+    // const signedId = await getSignedStreamId(id, env.CF_STREAM_SIGNING_KEY);
     return new Response(
-      JSON.stringify({
-        signedId: `${signedId}`,
-      }),
+      video,
+      // JSON.stringify({
+      //   signedId: `${signedId}`,
+      // }),
       {
         headers: {
           "content-type": "application/json",
@@ -52,14 +53,14 @@ export async function onRequestGet(context) {
     
     const videos = await Promise.all(
       filteredVideos.map(async (x) => {
-        const signedId = await getSignedStreamId(
-          x.uid,
-          env.CF_STREAM_SIGNING_KEY
-        );
+        // const signedId = await getSignedStreamId(
+        //   x.uid,
+        //   env.CF_STREAM_SIGNING_KEY
+        // );
         return {
           uid: x.uid,
           status: x.status,
-          thumbnail: `https://videodelivery.net/${signedId}/thumbnails/thumbnail.jpg`,
+          // thumbnail: `https://videodelivery.net/${env.CF_ACCOUNT_ID}/thumbnails/thumbnail.jpg`,
           meta: {
             name: x.meta.name,
           },
