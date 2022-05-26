9371c3d3e40757883075465a09cc185cc3bb4326
Wafa Bergaoui <wafa.bergaoui@sastec-group.com>
2022-05-26 08:00:52 +0100

    add video

--- components/views/video/template.jsx
+++ components/views/video/template.jsx
@@ -3,13 +3,13 @@ import { Stream } from "@cloudflare/stream-react";
 import { Container, Row, Card } from 'react-bootstrap'
 import S from './style.module.scss'
 
-const Video = () => {
+const Video = ({videoID}) => {
   return (
     <Container>
       <Row>
         <Card className={S.videoStyle}>
           <Card.Body>
-            <Stream controls src="548a1762f17f8ea1cda8344e0e48de7a" />
+            <Stream controls src={videoID} />
           </Card.Body>
         </Card>
       </Row>
--- pages/[id].js
+++ pages/[id].js
@@ -1,43 +1,14 @@
 import { Video } from "../components/views";
+import { useRouter } from 'next/router'
 
-// export async function getStaticPaths() {
-//   const res = await fetch(
-//     `https://cloudflare-test-ajl.pages.dev/api/videos?q=batman`,
-//     {
-//       headers: { accept: "application/json" },
-//     }
-//   );
-//   const data = await res.json();
-
-//   const paths = data.map((video) => ({
-//     id: video,
-//   }));
-//   return {
-//     paths,
-//     fallback: false,
-//   };
-// }
-
-// export async function getStaticProps({ params }) {
-//   const request = await fetch(
-//     `https://cloudflare-test-ajl.pages.dev/api/videos/${id}`
-//   );
-//   const videoID = await request.json();
-
-//   return {
-//     props: {
-//       videoID,
-//     },
-//   };
-// }
-
-function Home({ videoID }) {
-  //console.log("data", videoID);
+function Player() {
+  const router = useRouter()
+  
   return (
     <div>
-      <Video />
+      <Video videoID={router.query.id}/>
     </div>
   );
 }
 
-export default Home;
+export default Player;
